736b34dbda0d1082ce32941ec4548086185821b8
ahn-sj <jaesa5221@gmail.com>
2023-01-14 16:50:06 +0900

    feat: Member SecurityConfig and Not Yey Testing

--- src/main/java/airplainreservation/highestway/member/infrastructure/MemberRepository.java
+++ src/main/java/airplainreservation/highestway/member/infrastructure/MemberRepository.java
@@ -1,4 +1,10 @@
 package airplainreservation.highestway.member.infrastructure;
 
-public class MemberRepository {
+import airplainreservation.highestway.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 }
---
+++ src/main/java/airplainreservation/highestway/security/UserPrincipal.java
@@ -0,0 +1,54 @@
+package airplainreservation.highestway.security;
+
+import airplainreservation.highestway.member.domain.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserPrincipal implements UserDetails {
+    private Member member;
+
+    public UserPrincipal(Member member) {
+        this.member = member;
+    }
+
+    public static UserPrincipal create(Member member) {
+        return new UserPrincipal(member);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
