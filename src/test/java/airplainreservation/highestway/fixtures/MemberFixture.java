eb0a9661f987fae256c94f0b6ba9bd0c68edeb02
ahn-sj <jaesa5221@gmail.com>
2023-02-06 22:29:03 +0900

    test: Add fixtures package

---
+++ src/test/java/airplainreservation/highestway/fixtures/MemberFixture.java
@@ -0,0 +1,9 @@
+package airplainreservation.highestway.fixtures;
+
+public class MemberFixture {
+    private static final String USERNAME = "jae";
+    private static final String USERNAME1 = "jae1";
+    private static final String USERNAME2 = "jae2";
+
+    private static final String EMAIL = "jae@jae.com";
+}
